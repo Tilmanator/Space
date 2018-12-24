76be08744ba5f830bb37af4d191a5a098fb06f64
Tilman Lindig <tilman.lindig@rogers.com>
2018-12-23 19:18:29 -0500

    Add boost, count num files

--- main.cpp
+++ main.cpp
@@ -4,6 +4,14 @@
 #include <stdexcept>
 #include <string>
 #include <array>
+#include <string>
+
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+
+#define MAX_FILES 100
+
+namespace fs = boost::filesystem;
 
 std::string exec(const char *cmd) {
     std::array<char, 128> buffer{};
@@ -18,8 +26,28 @@ std::string exec(const char *cmd) {
     return result;
 }
 
-int main() {
-    std::string ret = exec("ls");
-    std::cout << ret << std::endl;
+int main(int argc, char *argv[]) {
+
+    fs::path p(fs::initial_path<fs::path>());
+    unsigned int cnt{0};
+    fs::directory_iterator end_iter;
+
+    // Count number of items in directory
+    for (fs::directory_iterator dir_itr(p); dir_itr != end_iter; ++dir_itr) {
+        ++cnt;
+    }
+
+    if (cnt > MAX_FILES) {
+        std::cout << "Limiting to biggest " << MAX_FILES << " files/dirs." << std::endl;
+    }
+
+    if (argc > 1 && !strcmp(argv[1], "--no-gui")) {
+        std::cout << "No GUI, sorting " << std::endl;
+        std::string ret = exec("du -sh -- *;");
+        std::cout << ret << std::endl;
+        return 0;
+    }
+
+    std::cout << cnt << std::endl;
     return 0;
 }
